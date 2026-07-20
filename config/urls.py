da8e7be80b0e8624311db4ac5b00c199a14af809
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-07-21 03:12:14 +0330

    Serve uploaded SAMS offer media

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,13 @@
+from django.conf import settings
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
     path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("", include("cards.urls")),
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
 ]
