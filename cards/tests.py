d7c3892b7d7dee953c420315388a704cdaaef78e
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-07-20 17:29:20 +0330

    Test member numbers and generated bills

--- cards/tests.py
+++ cards/tests.py
@@ -18,12 +18,21 @@ class WalletServiceTests(TestCase):
         Membership.objects.create(user=self.staff, business=self.business, role=Membership.Role.STAFF)
         self.wallet = Wallet.objects.create(business=self.business, owner=self.customer, display_name="Customer")
 
+    def test_wallet_gets_unique_eight_digit_member_number(self):
+        second_wallet = Wallet.objects.create(business=self.business, display_name="Second Customer")
+        self.assertEqual(len(self.wallet.member_number), 8)
+        self.assertTrue(self.wallet.member_number.isdigit())
+        self.assertNotEqual(self.wallet.member_number, second_wallet.member_number)
+
     def test_topup_and_purchase_update_balance_and_ledger(self):
-        post_wallet_entry(wallet=self.wallet, entry_type=LedgerEntry.Type.TOPUP, amount="100", actor=self.owner)
-        post_wallet_entry(wallet=self.wallet, entry_type=LedgerEntry.Type.PURCHASE, amount="30", actor=self.staff)
+        topup = post_wallet_entry(wallet=self.wallet, entry_type=LedgerEntry.Type.TOPUP, amount="100", actor=self.owner)
+        purchase = post_wallet_entry(wallet=self.wallet, entry_type=LedgerEntry.Type.PURCHASE, amount="30", actor=self.staff)
         self.wallet.refresh_from_db()
         self.assertEqual(str(self.wallet.balance), "70.00")
         self.assertEqual(self.wallet.ledger_entries.count(), 2)
+        self.assertTrue(topup.bill_number.startswith("B-"))
+        self.assertTrue(purchase.bill_number.startswith("B-"))
+        self.assertNotEqual(topup.bill_number, purchase.bill_number)
 
     def test_purchase_cannot_make_balance_negative(self):
         with self.assertRaises(ValidationError):
@@ -65,6 +74,8 @@ class CustomerRegistrationTests(TestCase):
         self.assertEqual(wallet.display_name, "Lena Sommer")
         self.assertEqual(wallet.phone, "+49 160 1234567")
         self.assertEqual(str(wallet.balance), "0.00")
+        self.assertEqual(len(wallet.member_number), 8)
+        self.assertTrue(wallet.member_number.isdigit())
         self.assertEqual(int(self.client.session["_auth_user_id"]), user.pk)
 
     def test_duplicate_email_is_rejected(self):
@@ -113,3 +124,49 @@ class ManagerQrScanTests(TestCase):
         self.client.force_login(self.owner)
         response = self.client.get(reverse("manager_wallet_scan"), {"token": "not-a-member-code"})
         self.assertRedirects(response, reverse("manager_dashboard"))
+
+
+class BillAccessTests(TestCase):
+    def setUp(self):
+        User = get_user_model()
+        self.business = Business.objects.create(name="SAMS CLUB LOUNGE", slug="bill-lounge")
+        self.other_business = Business.objects.create(name="Other Lounge", slug="bill-other")
+        self.owner = User.objects.create_user(username="bill-owner", password="test")
+        self.staff = User.objects.create_user(username="bill-staff", password="test")
+        self.customer = User.objects.create_user(username="bill-customer", password="test")
+        self.outsider = User.objects.create_user(username="bill-outsider", password="test")
+        self.other_staff = User.objects.create_user(username="other-staff", password="test")
+        Membership.objects.create(user=self.owner, business=self.business, role=Membership.Role.OWNER)
+        Membership.objects.create(user=self.staff, business=self.business, role=Membership.Role.STAFF)
+        Membership.objects.create(user=self.other_staff, business=self.other_business, role=Membership.Role.STAFF)
+        self.wallet = Wallet.objects.create(business=self.business, owner=self.customer, display_name="Bill Customer")
+        self.entry = post_wallet_entry(
+            wallet=self.wallet,
+            entry_type=LedgerEntry.Type.TOPUP,
+            amount="50.00",
+            actor=self.owner,
+            description="Cash top-up",
+        )
+
+    def test_customer_can_open_own_bill(self):
+        self.client.force_login(self.customer)
+        response = self.client.get(reverse("bill_detail", args=[self.entry.pk]))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.entry.bill_number)
+        self.assertContains(response, self.wallet.member_number)
+
+    def test_owner_and_staff_can_open_business_bill(self):
+        for user in (self.owner, self.staff):
+            self.client.force_login(user)
+            response = self.client.get(reverse("bill_detail", args=[self.entry.pk]))
+            self.assertEqual(response.status_code, 200)
+
+    def test_unrelated_customer_cannot_open_bill(self):
+        self.client.force_login(self.outsider)
+        response = self.client.get(reverse("bill_detail", args=[self.entry.pk]))
+        self.assertEqual(response.status_code, 403)
+
+    def test_staff_from_another_business_cannot_open_bill(self):
+        self.client.force_login(self.other_staff)
+        response = self.client.get(reverse("bill_detail", args=[self.entry.pk]))
+        self.assertEqual(response.status_code, 403)
