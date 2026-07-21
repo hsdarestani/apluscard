8eff45faefd711b72ec6fa434e9f83ace7e7e565
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-07-21 14:38:28 +0330

    Apple-Anmelderouten einbinden

--- config/urls.py
+++ config/urls.py
@@ -4,10 +4,15 @@ from django.contrib.auth import views as auth_views
 from django.urls import include, path, re_path
 from django.views.static import serve
 
+admin.site.site_header = "SAMS Verwaltung"
+admin.site.site_title = "SAMS Verwaltung"
+admin.site.index_title = "Übersicht"
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
     path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("accounts/", include("allauth.urls")),
     path("", include("cards.urls")),
     re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
 ]
