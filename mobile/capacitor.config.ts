334dc67ffeb0a95a957cc2afc3fc01b1bdb6f88d
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-07-24 15:31:23 +0330

    Package ID را با Play Console هماهنگ کن

--- mobile/capacitor.config.ts
+++ mobile/capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'de.aplussolution.samscard',
+  appId: 'de.aplussolution.apluscard',
   appName: 'SAMS Card',
   webDir: 'www',
   server: {
