300f21b1a019d918ec2aa060df508389b06be016
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-07-24 16:25:37 +0330

    Push Callback و Status Bar آیفون را نهایی کن

--- mobile/ci/prepare-ios-release.rb
+++ mobile/ci/prepare-ios-release.rb
@@ -22,9 +22,11 @@ project_path = File.join(ios_root, 'App.xcodeproj')
 app_dir = File.join(ios_root, 'App')
 info_plist_path = File.join(app_dir, 'Info.plist')
 entitlements_path = File.join(app_dir, 'App.entitlements')
+app_delegate_path = File.join(app_dir, 'AppDelegate.swift')
 
 abort("Xcode-Projekt fehlt: #{project_path}") unless File.directory?(project_path)
 abort("Info.plist fehlt: #{info_plist_path}") unless File.file?(info_plist_path)
+abort("AppDelegate.swift fehlt: #{app_delegate_path}") unless File.file?(app_delegate_path)
 
 entitlements = {
   'aps-environment' => 'production',
@@ -39,7 +41,28 @@ info_plist = Xcodeproj::Plist.read_from_path(info_plist_path)
 background_modes = Array(info_plist['UIBackgroundModes'])
 background_modes << 'remote-notification' unless background_modes.include?('remote-notification')
 info_plist['UIBackgroundModes'] = background_modes
+info_plist['UIViewControllerBasedStatusBarAppearance'] = false
+info_plist['UIStatusBarStyle'] = 'UIStatusBarStyleLightContent'
 Xcodeproj::Plist.write_to_path(info_plist, info_plist_path)
+
+app_delegate = File.read(app_delegate_path)
+unless app_delegate.include?('capacitorDidRegisterForRemoteNotifications')
+  File.open(app_delegate_path, 'a') do |file|
+    file.write <<~SWIFT
+
+      // Bridge APNs callbacks to @capacitor/push-notifications.
+      extension AppDelegate {
+          func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+              NotificationCenter.default.post(name: .capacitorDidRegisterForRemoteNotifications, object: deviceToken)
+          }
+
+          func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+              NotificationCenter.default.post(name: .capacitorDidFailToRegisterForRemoteNotifications, object: error)
+          }
+      }
+    SWIFT
+  end
+end
 
 project = Xcodeproj::Project.open(project_path)
 target = project.targets.find { |candidate| candidate.name == 'App' }
