b418f3e43afaeaf61d55d5de4222754fbf493fe1
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-07-30 21:06:22 +0330

    Remove runtime QR scanner CDN from CSP

--- cards/security_middleware.py
+++ cards/security_middleware.py
@@ -23,7 +23,7 @@ class SecurityHeadersMiddleware:
             "img-src 'self' data: blob: https://appleid.cdn-apple.com; "
             "font-src 'self' data:; "
             "style-src 'self' 'unsafe-inline'; "
-            "script-src 'self' 'unsafe-inline' https://unpkg.com; "
+            "script-src 'self' 'unsafe-inline'; "
             "connect-src 'self'; "
             "media-src 'self' blob:; "
             "form-action 'self' https://appleid.apple.com"
