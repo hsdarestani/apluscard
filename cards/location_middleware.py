3422a3bef9d700a7d6fd99b95098e1af64ffa21a
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-08-07 11:24:01 +0330

    Allow customers to confirm pending payments without location redirect

--- cards/location_middleware.py
+++ cards/location_middleware.py
@@ -18,6 +18,7 @@ class CustomerLocationSelectionMiddleware:
         "/apps/",
         "/rechtliches-bestaetigen/",
         "/customer/standort-waehlen/",
+        "/customer/payments/",
         "/customer/apple-wallet/",
         "/wallet/download/",
         "/mitteilungen/",
